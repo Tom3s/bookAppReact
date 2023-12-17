44e916c8ec53e4cc4320d059102479d1c90a6ad1
915_Nagy_Matyas <tomesjerry3@gmail.com>
2023-12-17 14:55:49 +0200

    Local DB added

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { Fragment, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import { Book, BookStatus, Genre } from "./src/Model";
 import { Alert, Button, Pressable, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { ListView } from "./src/ListView";
@@ -9,163 +9,180 @@ import { BookRepository } from "./src/BookRepository";
 interface AppProps { }
 const initialBooks: Book[] = [
 	{
-	  id: 1,
-	  title: "To Kill a Mockingbird",
-	  author: "Harper Lee",
-	  description: "A classic novel exploring racial injustice and moral growth in the American South.",
-	  genre: Genre.Drama,
-	  nrPages: 281,
-	  status: BookStatus.Read,
+		id: 1,
+		title: "To Kill a Mockingbird",
+		author: "Harper Lee",
+		description: "A classic novel exploring racial injustice and moral growth in the American South.",
+		genre: Genre.Drama,
+		nrPages: 281,
+		status: BookStatus.Read,
 	},
 	{
-	  id: 2,
-	  title: "1984",
-	  author: "George Orwell",
-	  description: "A dystopian novel depicting a totalitarian regime and the consequences of extreme political ideologies.",
-	  genre: Genre.Dystopian,
-	  nrPages: 328,
-	  status: BookStatus.Read,
+		id: 2,
+		title: "1984",
+		author: "George Orwell",
+		description: "A dystopian novel depicting a totalitarian regime and the consequences of extreme political ideologies.",
+		genre: Genre.Dystopian,
+		nrPages: 328,
+		status: BookStatus.Read,
 	},
 	{
-	  id: 3,
-	  title: "The Great Gatsby",
-	  author: "F. Scott Fitzgerald",
-	  description: "A novel set in the Roaring Twenties, exploring themes of wealth, love, and the American Dream.",
-	  genre: Genre.Romance,
-	  nrPages: 180,
-	  status: BookStatus.Read,
+		id: 3,
+		title: "The Great Gatsby",
+		author: "F. Scott Fitzgerald",
+		description: "A novel set in the Roaring Twenties, exploring themes of wealth, love, and the American Dream.",
+		genre: Genre.Romance,
+		nrPages: 180,
+		status: BookStatus.Read,
 	},
 	{
-	  id: 4,
-	  title: "Pride and Prejudice",
-	  author: "Jane Austen",
-	  description: "A classic novel exploring love, class, and social expectations in early 19th-century England.",
-	  genre: Genre.Romance,
-	  nrPages: 279,
-	  status: BookStatus.Read,
+		id: 4,
+		title: "Pride and Prejudice",
+		author: "Jane Austen",
+		description: "A classic novel exploring love, class, and social expectations in early 19th-century England.",
+		genre: Genre.Romance,
+		nrPages: 279,
+		status: BookStatus.Read,
 	},
 	{
-	  id: 5,
-	  title: "The Catcher in the Rye",
-	  author: "J.D. Salinger",
-	  description: "A novel following the experiences of a teenager in New York City, exploring themes of alienation and identity.",
-	  genre: Genre.Drama,
-	  nrPages: 277,
-	  status: BookStatus.Read,
+		id: 5,
+		title: "The Catcher in the Rye",
+		author: "J.D. Salinger",
+		description: "A novel following the experiences of a teenager in New York City, exploring themes of alienation and identity.",
+		genre: Genre.Drama,
+		nrPages: 277,
+		status: BookStatus.Read,
 	},
 	{
-	  id: 6,
-	  title: "The Girl with the Dragon Tattoo",
-	  author: "Stieg Larsson",
-	  description: "A mystery thriller novel featuring investigative journalist Mikael Blomkvist and hacker Lisbeth Salander.",
-	  genre: Genre.Mystery,
-	  nrPages: 590,
-	  status: BookStatus.Reading,
+		id: 6,
+		title: "The Girl with the Dragon Tattoo",
+		author: "Stieg Larsson",
+		description: "A mystery thriller novel featuring investigative journalist Mikael Blomkvist and hacker Lisbeth Salander.",
+		genre: Genre.Mystery,
+		nrPages: 590,
+		status: BookStatus.Reading,
 	},
 	{
-	  id: 7,
-	  title: "The Hunger Games",
-	  author: "Suzanne Collins",
-	  description: "A dystopian novel set in a post-apocalyptic world where young people are forced to participate in a televised death match.",
-	  genre: Genre.Dystopian,
-	  nrPages: 374,
-	  status: BookStatus.Unread,
+		id: 7,
+		title: "The Hunger Games",
+		author: "Suzanne Collins",
+		description: "A dystopian novel set in a post-apocalyptic world where young people are forced to participate in a televised death match.",
+		genre: Genre.Dystopian,
+		nrPages: 374,
+		status: BookStatus.Unread,
 	},
 	{
-	  id: 8,
-	  title: "The Da Vinci Code",
-	  author: "Dan Brown",
-	  description: "A mystery thriller novel that follows symbologist Robert Langdon in a quest for hidden secrets.",
-	  genre: Genre.Mystery,
-	  nrPages: 454,
-	  status: BookStatus.Reading,
+		id: 8,
+		title: "The Da Vinci Code",
+		author: "Dan Brown",
+		description: "A mystery thriller novel that follows symbologist Robert Langdon in a quest for hidden secrets.",
+		genre: Genre.Mystery,
+		nrPages: 454,
+		status: BookStatus.Reading,
 	},
 	{
-	  id: 9,
-	  title: "The Shining",
-	  author: "Stephen King",
-	  description: "A horror novel that tells the story of a family's winter stay at an isolated hotel, haunted by supernatural forces.",
-	  genre: Genre.Horror,
-	  nrPages: 447,
-	  status: BookStatus.Unread,
+		id: 9,
+		title: "The Shining",
+		author: "Stephen King",
+		description: "A horror novel that tells the story of a family's winter stay at an isolated hotel, haunted by supernatural forces.",
+		genre: Genre.Horror,
+		nrPages: 447,
+		status: BookStatus.Unread,
 	},
 	{
-	  id: 10,
-	  title: "The Martian",
-	  author: "Andy Weir",
-	  description: "A science fiction novel about an astronaut stranded on Mars and his struggle for survival.",
-	  genre: Genre.ScienceFiction,
-	  nrPages: 369,
-	  status: BookStatus.Unread,
+		id: 10,
+		title: "The Martian",
+		author: "Andy Weir",
+		description: "A science fiction novel about an astronaut stranded on Mars and his struggle for survival.",
+		genre: Genre.ScienceFiction,
+		nrPages: 369,
+		status: BookStatus.Unread,
 	},
 	{
-	  id: 11,
-	  title: "Gone Girl",
-	  author: "Gillian Flynn",
-	  description: "A psychological thriller novel that explores the complexities of a marriage and a mysterious disappearance.",
-	  genre: Genre.Thriller,
-	  nrPages: 415,
-	  status: BookStatus.Unread,
+		id: 11,
+		title: "Gone Girl",
+		author: "Gillian Flynn",
+		description: "A psychological thriller novel that explores the complexities of a marriage and a mysterious disappearance.",
+		genre: Genre.Thriller,
+		nrPages: 415,
+		status: BookStatus.Unread,
 	},
 	{
-	  id: 12,
-	  title: "Jurassic Park",
-	  author: "Michael Crichton",
-	  description: "A science fiction novel about the disastrous attempt to create a theme park with cloned dinosaurs.",
-	  genre: Genre.ScienceFiction,
-	  nrPages: 448,
-	  status: BookStatus.Unread,
+		id: 12,
+		title: "Jurassic Park",
+		author: "Michael Crichton",
+		description: "A science fiction novel about the disastrous attempt to create a theme park with cloned dinosaurs.",
+		genre: Genre.ScienceFiction,
+		nrPages: 448,
+		status: BookStatus.Unread,
 	},
 	{
-	  id: 13,
-	  title: "The Road",
-	  author: "Cormac McCarthy",
-	  description: "A post-apocalyptic novel following a father and son's journey through a desolate landscape.",
-	  genre: Genre.Dystopian,
-	  nrPages: 241,
-	  status: BookStatus.Unread,
+		id: 13,
+		title: "The Road",
+		author: "Cormac McCarthy",
+		description: "A post-apocalyptic novel following a father and son's journey through a desolate landscape.",
+		genre: Genre.Dystopian,
+		nrPages: 241,
+		status: BookStatus.Unread,
 	},
 	{
-	  id: 14,
-	  title: "The Girl on the Train",
-	  author: "Paula Hawkins",
-	  description: "A psychological thriller novel centered around the mysterious disappearance of a woman.",
-	  genre: Genre.Thriller,
-	  nrPages: 336,
-	  status: BookStatus.Unread,
+		id: 14,
+		title: "The Girl on the Train",
+		author: "Paula Hawkins",
+		description: "A psychological thriller novel centered around the mysterious disappearance of a woman.",
+		genre: Genre.Thriller,
+		nrPages: 336,
+		status: BookStatus.Unread,
 	},
 	{
-	  id: 15,
-	  title: "The Alchemist",
-	  author: "Paulo Coelho",
-	  description: "A philosophical novel following the journey of a young shepherd as he pursues his dreams.",
-	  genre: Genre.Adventure,
-	  nrPages: 163,
-	  status: BookStatus.Read,
+		id: 15,
+		title: "The Alchemist",
+		author: "Paulo Coelho",
+		description: "A philosophical novel following the journey of a young shepherd as he pursues his dreams.",
+		genre: Genre.Adventure,
+		nrPages: 163,
+		status: BookStatus.Read,
 	},
-  ];
-  
-const bookRepo = new BookRepository(initialBooks);
+];
+
+const bookRepo = new BookRepository();
 
 const App: React.FC<AppProps> = () => {
 
 
-	const [books, setBooks] = useState<Book[]>(bookRepo.getAllBooks());
+	const [books, setBooks] = useState<Book[]>([]);
 	const [currentBook, setCurrentBook] = useState<Book | null>(null);
+	const [editing, setEditing] = useState<number | null>(null);
+	const [creating, setCreating] = useState<boolean>(false);
 
-	const handleCreate = (newBook: Book) => {
-		bookRepo.add(newBook);
-		setBooks(bookRepo.getAllBooks());
-		setCurrentBook(null);
+	useEffect(() => {
+		const fetchBooks = async () => {
+			const allBooks = await bookRepo.getAllBooks();
+			setBooks(allBooks);
+		};
+
+		fetchBooks();
+	}, []);
+
+	const handleCreate = async (newBook: Book) => {
+		await bookRepo.add(newBook);
+		const allBooks = await bookRepo.getAllBooks();
+		setBooks(allBooks);
+		// setCurrentBook(null);
+		setCreating(false);
+		setEditing(null);
 	};
 
-	const handleUpdate = (updatedBook: Book) => {
-		bookRepo.update(updatedBook);
-		setBooks(bookRepo.getAllBooks());
-		setCurrentBook(null);
+	const handleUpdate = async (updatedBook: Book) => {
+		await bookRepo.update(updatedBook);
+		const allBooks = await bookRepo.getAllBooks();
+		setBooks(allBooks);
+		// setCurrentBook(null);
+		setCreating(false);
+		setEditing(null);
 	};
 
-	const handleDelete = (id: number) => {
+	const handleDelete = async (id: number) => {
 		Alert.alert(
 			'Confirm delete',
 			'Are you sure you want to delete this book?',
@@ -176,10 +193,13 @@ const App: React.FC<AppProps> = () => {
 				},
 				{
 					text: 'Delete',
-					onPress: () => {
-						bookRepo.delete(id);
-						setBooks(bookRepo.getAllBooks());
-						setCurrentBook(null);
+					onPress: async () => {
+						await bookRepo.delete(id);
+						const allBooks = await bookRepo.getAllBooks();
+						setBooks(allBooks);
+						// setCurrentBook(null);
+						setCreating(false);
+						setEditing(null);
 					}
 				}
 			],
@@ -190,35 +210,31 @@ const App: React.FC<AppProps> = () => {
 	return (
 		<View style={styles.container}>
 			{
-				currentBook === null &&
+				editing === null && creating === false &&
 				<Fragment>
-
-					{/* <TouchableOpacity
-						style={styles.createButton}
-					>
-					<Pressable
-					style={styles.createButton}
-					onPress={() => console.log("Pressed")}
-					>
-					<Text>Create</Text>
-					</Pressable>
-				</TouchableOpacity> */}
 					<ListView
 						data={books}
-						onPressItem={(item) => currentBook === null ? setCurrentBook(item) : setCurrentBook(null)}
+						onPressItem={(item) => {
+							setEditing(item.id)
+							setCurrentBook(item)
+							setCreating(false)
+						}}
 						onDeleteItem={(id) => handleDelete(id)}
 					/>
 					<Button
 						title="Create"
-						onPress={() => setCurrentBook({ id: 0 } as Book)}
+						onPress={() => {
+							setCreating(true);
+							setEditing(null);
+						}}
 					/>
 				</Fragment>
 			}
-			{currentBook && currentBook.id === 0 && (
-				<CreateView onSubmit={handleCreate} onCancel={() => setCurrentBook(null)} />
+			{editing === null && creating && (
+				<CreateView onSubmit={handleCreate} onCancel={() => {setCreating(false); setEditing(null)}} />
 			)}
-			{currentBook && currentBook.id !== 0 && (
-				<UpdateView item={currentBook} onSubmit={handleUpdate} onCancel={() => setCurrentBook(null)} />
+			{!creating && editing !== null && currentBook !== null && (
+				<UpdateView item={currentBook} onSubmit={handleUpdate} onCancel={() => {setCreating(false); setEditing(null)}} />
 			)}
 		</View>
 	)
--- src/BookRepository.tsx
+++ src/BookRepository.tsx
@@ -1,31 +1,122 @@
 import { Book } from "./Model";
+import SQLite from 'react-native-sqlite-storage';
 
 export class BookRepository {
-	constructor(
-		private books: Book[],
-	) {}
+	db: SQLite.SQLiteDatabase;
+	constructor() {
+		this.db = SQLite.openDatabase(
+		  {
+			name: 'MainDB',
+			location: 'default',
+		  },
+		  () => {
+			this.db.transaction((tx) => {
+			  tx.executeSql(
+				`CREATE TABLE IF NOT EXISTS Books
+				(id INTEGER PRIMARY KEY NOT NULL,
+				title TEXT,
+				author TEXT,
+				description TEXT,
+				genre TEXT,
+				nrPages INTEGER,
+				status TEXT)`,
+				[],
+				() => { console.log('Table initialized'); },
+				error => { console.log(error); }
+			  );
+			});
+		  },
+		  error => { console.log(error); }
+		);
+	  }
 
-	getAllBooks(): Book[] {
-		return this.books;
-	}
+	// getAllBooks(): Book[] {
+	// 	return this.books;
+	// }
 
-	add(book: Book): number {
-		const lastBook = this.books[this.books.length - 1];
-		if (lastBook) {
-			book.id = lastBook.id + 1;
-		} else {
-			book.id = 1;
-		}
-		this.books.push(book);
-		return book.id;
-	}
+	getAllBooks(): Promise<Book[]> {
+		return new Promise((resolve, reject) => {
+		  this.db.transaction((tx) => {
+			tx.executeSql('SELECT * FROM Books', [], (tx, results) => {
+			  let books: Book[] = [];
+			  for (let i = 0; i < results.rows.length; i++) {
+				books.push(results.rows.item(i));
+			  }
+			  resolve(books);
+			});
+		  });
+		});
+	  }
 
-	update(book: Book): void {
-		const index = this.books.findIndex((b) => b.id === book.id);
-		this.books[index] = book;
-	}
+	getBookById(id: number): Promise<Book> {
+		return new Promise((resolve, reject) => {
+		  this.db.transaction((tx) => {
+			tx.executeSql('SELECT * FROM Books WHERE id = ?', [id], (tx, results) => {
+			  resolve(results.rows.item(0));
+			});
+		  });
+		});
+	  }
 
-	delete(id: number): void {
-		this.books = this.books.filter((b) => b.id !== id);
-	}
+	// add(book: Book): number {
+	// 	const lastBook = this.books[this.books.length - 1];
+	// 	if (lastBook) {
+	// 		book.id = lastBook.id + 1;
+	// 	} else {
+	// 		book.id = 1;
+	// 	}
+	// 	this.books.push(book);
+	// 	return book.id;
+	// }
+
+	add(book: Book): Promise<number> {
+		return new Promise((resolve, reject) => {
+		  this.db.transaction((tx) => {
+			tx.executeSql(
+			  'INSERT INTO Books (title, author, description, genre, nrPages, status) VALUES (?, ?, ?, ?, ?, ?)',
+			  [book.title, book.author, book.description, book.genre, book.nrPages, book.status],
+			  (tx, results) => {
+				resolve(results.insertId);
+			  }
+			);
+		  });
+		});
+	  }
+
+	// update(book: Book): void {
+	// 	const index = this.books.findIndex((b) => b.id === book.id);
+	// 	this.books[index] = book;
+	// }
+
+	update(book: Book): Promise<void> {
+		return new Promise((resolve, reject) => {
+		  this.db.transaction((tx) => {
+			tx.executeSql(
+			  'UPDATE Books SET title = ?, author = ?, description = ?, genre = ?, nrPages = ?, status = ? WHERE id = ?',
+			  [book.title, book.author, book.description, book.genre, book.nrPages, book.status, book.id],
+			  (tx, results) => {
+				resolve();
+			  }
+			);
+		  });
+		});
+	  }
+
+	// delete(id: number): void {
+	// 	this.books = this.books.filter((b) => b.id !== id);
+	// }
+
+	delete(id: number): Promise<void> {
+		return new Promise((resolve, reject) => {
+		  this.db.transaction((tx) => {
+			tx.executeSql(
+			  'DELETE FROM Books WHERE id = ?',
+			  [id],
+			  (tx, results) => {
+				resolve();
+			  }
+			);
+		  });
+		});
+	  }
 }
